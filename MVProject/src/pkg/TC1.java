cbb0305128eb6acaf064de57b1a6e5f1045713f7
Vipin <vipin.dhiman21@gmail.com>
2022-05-28 22:04:40 +0530

    add

---
+++ MVProject/src/pkg/TC1.java
@@ -0,0 +1,11 @@
+package pkg;
+
+import org.testng.annotations.Test;
+
+public class TC1 {
+
+	@Test
+	public void test() {
+		System.out.println("TC1");
+	}
+}
---
+++ MVProject/src/pkg/TC2.java
@@ -0,0 +1,11 @@
+package pkg;
+
+import org.testng.annotations.Test;
+
+public class TC2 {
+
+	@Test
+	public void testing() {
+		System.out.println("TC2");
+	}
+}
